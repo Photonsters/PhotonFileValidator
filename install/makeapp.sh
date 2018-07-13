158f508c3d947adb8d33a0ec9a6b47ef06481838
bonosoft <bo.n@rgaard.net>
2018-07-13 11:26:02 +0200

    Add osx installer with JRE included

---
+++ install/makeapp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+jar2app ../out/artifacts/PhotonFileValidator.jar -i img/icons/photonsters.icns -n "Photon File Validator" -v 1.2.0 -s 1.2.0 -c "(C) Copyright by Bonosoft" -j "-Xmx5g -Xms1g"
+
--- makeapp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-jar2app out/artifacts/PhotonFileValidator.jar -i install/img/photonsters.icns -n "Photon File Validator" -v 1.2.0 -s 1.2.0 -c "(C) Copyright by Bonosoft" -j "-Xmx5g -Xms1g"
-
